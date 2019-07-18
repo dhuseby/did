7ac33b9976d57d098bb93facf1bc053129f8ead4
Dave Huseby <dhuseby@linuxfoundation.org>
2019-07-18 16:43:45 -0700

    moving towards a normalized interface
    Signed-off-by: Dave Huseby <dhuseby@linuxfoundation.org>

--- src/fields/subject.rs
+++ src/fields/subject.rs
@@ -1,16 +1,32 @@
 use serde_derive::{Serialize, Deserialize};
+use std::str::FromStr;
+use void::Void;
 
 #[derive(Serialize, Deserialize, Default, Debug, PartialEq)]
 #[serde(transparent)]
 pub struct Subject(String);
 
 impl Subject {
+    /*
     pub fn new(s: &str) -> Self {
         Subject(s.to_owned())
     }
+    */
 
     pub fn as_str(&self) -> &str {
         &self.0
+    }
+
+    pub fn is_missing(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl FromStr for Subject {
+    type Err = Void;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Subject(s.to_owned()))
     }
 }
 
